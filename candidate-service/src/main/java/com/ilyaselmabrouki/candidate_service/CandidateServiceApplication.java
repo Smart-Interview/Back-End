d20cc2d6f7b8c90869795831cccd0c105c3a6aca
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-08-23 09:29:26 +0100

    Create candidate service

---
+++ candidate-service/src/main/java/com/ilyaselmabrouki/candidate_service/CandidateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ilyaselmabrouki.candidate_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateServiceApplication.class, args);
+	}
+
+}
