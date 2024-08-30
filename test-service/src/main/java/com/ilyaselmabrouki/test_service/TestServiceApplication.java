b4d3a974e75b00db8bf29c039588baa721724b98
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-08-30 15:34:53 +0100

    Configured test management microservice

--- test-service/src/main/java/com/ilyaselmabrouki/test_service/TestServiceApplication.java
+++ test-service/src/main/java/com/ilyaselmabrouki/test_service/TestServiceApplication.java
@@ -2,8 +2,10 @@ package com.ilyaselmabrouki.test_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TestServiceApplication {
 
 	public static void main(String[] args) {
