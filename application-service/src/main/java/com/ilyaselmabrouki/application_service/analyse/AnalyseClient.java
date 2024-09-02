e95b523a7e4cd4e5bf4b37713b471a07564a2284
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-09-02 09:50:15 +0100

    Update request entity to analyse service

--- application-service/src/main/java/com/ilyaselmabrouki/application_service/analyse/AnalyseClient.java
+++ application-service/src/main/java/com/ilyaselmabrouki/application_service/analyse/AnalyseClient.java
@@ -1,14 +1,18 @@
 package com.ilyaselmabrouki.application_service.analyse;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(
         name = "analyse-service",
         url = "${application.config.analyse-url}"
 )
 public interface AnalyseClient {
-    @PostMapping("/analyze-cv")
-    AnalyseResponse analyzeCv(@RequestBody AnalyseRequest request);
+    @PostMapping(value = "/analyze-cv", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    AnalyseResponse analyzeCv(@RequestPart("offerId") Integer applicationId,
+                              @RequestPart("offerId") Integer offerId,
+                              @RequestPart("cv") MultipartFile cv);
 }
