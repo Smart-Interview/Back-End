f6259f0b2e4cdfe9b5ed6cf88e8f413efa52d73a
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-09-05 20:44:16 +0100

    Resolved web dependency issue

--- gateway/src/main/java/com/ilyaselmabrouki/gateway/security/SecurityConfig.java
+++ gateway/src/main/java/com/ilyaselmabrouki/gateway/security/SecurityConfig.java
@@ -3,43 +3,25 @@ package com.ilyaselmabrouki.gateway.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
-    private ClientRegistrationRepository clientRegistrationRepository;
-
-    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
-        this.clientRegistrationRepository = clientRegistrationRepository;
-    }
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(Customizer.withDefaults())
-                .authorizeHttpRequests(ar->ar.requestMatchers("/").permitAll())
-                .authorizeHttpRequests(ar-> ar.anyRequest().authenticated())
-                .oauth2Login(Customizer.withDefaults())
-                .logout((logout) -> logout
-                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
-                        .logoutSuccessUrl("/api/v1/auth/").permitAll()
-                        .clearAuthentication(true)
-                        .deleteCookies("JSESSIONID"))
-                .build();
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(auth -> auth.anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
     }
 
-    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
-        final OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
-                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
-        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}?logoutsuccess=true");
-        return oidcLogoutSuccessHandler;
-    }
 }
