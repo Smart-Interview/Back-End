a7af2b207a4c791b1ba36949981c1e58f2a12b83
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-09-20 14:38:25 +0100

    Configured CORS in services

---
+++ company-service/src/main/java/com/elabidisoufiane/sosouca/security/GlobalCorsConfig.java
@@ -0,0 +1,24 @@
+package com.elabidisoufiane.sosouca.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*");  // Allow all origins
+        corsConfig.addAllowedHeader("*");  // Allow all headers
+        corsConfig.addAllowedMethod("*");  // Allow all HTTP methods
+        corsConfig.setAllowCredentials(true);  // Allow credentials (for cookies, tokens)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);  // Apply to all paths
+
+        return new CorsFilter(source);
+    }
+}
